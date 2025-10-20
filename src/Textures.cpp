bb4b6c8941c386936692bf7903f058f7be791e06
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2025-10-20 23:30:47 +0200

    Sprite Qol format switch
    Replaces various image assets from BMP, PNG, and JPG formats to TGA format for consistency. Updates texture loading paths in Textures.cpp to use the new TGA files. Adds global.ini configuration file.

--- src/Textures.cpp
+++ src/Textures.cpp
@@ -8,14 +8,14 @@ namespace Textures
 
     void Load()
     {
-        textures.push_back(TextureStruct(TextureEnum_None, LoadTexture("./graphics/missing.jpg")));
-        textures.push_back(TextureStruct(CursorHover, LoadTexture("./graphics/hover.png")));
-        textures.push_back(TextureStruct(CursorGrab, LoadTexture("./graphics/grab.png")));
-        textures.push_back(TextureStruct(texture_Home, LoadTexture("./graphics/home.bmp")));
-        textures.push_back(TextureStruct(texture_hallway, LoadTexture("./graphics/hallway.png")));
-        textures.push_back(TextureStruct(Texture_outside_home, LoadTexture("./graphics/outside.png")));
-        textures.push_back(TextureStruct(Eye, LoadTexture("./graphics/eye.bmp")));
-        textures.push_back(TextureStruct(Eye2, LoadTexture("./graphics/eye2.bmp")));
+        textures.push_back(TextureStruct(TextureEnum_None, LoadTexture("./graphics/missing.tga")));
+        textures.push_back(TextureStruct(CursorHover, LoadTexture("./graphics/hover.tga")));
+        textures.push_back(TextureStruct(CursorGrab, LoadTexture("./graphics/grab.tga")));
+        textures.push_back(TextureStruct(texture_Home, LoadTexture("./graphics/home.tga")));
+        textures.push_back(TextureStruct(texture_hallway, LoadTexture("./graphics/hallway.tga")));
+        textures.push_back(TextureStruct(Texture_outside_home, LoadTexture("./graphics/outside.tga")));
+        textures.push_back(TextureStruct(Eye, LoadTexture("./graphics/eye.tga")));
+        textures.push_back(TextureStruct(Eye2, LoadTexture("./graphics/eye2.tga")));
     }
     void Unload()
     {
